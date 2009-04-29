77dfbaaf2ed18e57dd82af06fe611d48fb63c70b
Pavel Panchekha <pavpanchekha@gmail.com>
2009-04-29 11:40:41 -0400

    Initial commit, before style is separated out

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+$a = str_ireplace("\n", "", trim(htmlspecialchars($_POST["space"])));
+if(strlen($a) != 0) {
+	file_put_contents("probs.txt", "[prob]".$a."[/prob]\n", FILE_APPEND);
+	$submitted = 1;
+	
+	// increment
+	$x = intval(file_get_contents("count.txt"));
+	$x++;
+	file_put_contents("count.txt", $x."\n");
+}
+?>
+<html>
+<head>
+<title>Problem Submissions</title>
+<style type="text/css">
+body {
+	font: 12pt "Courier New";
+	width: 800px;
+}
+
+textarea {
+	font: 12pt "Courier New";
+}
+</style>
+<script type="text/javascript">
+function changeBg(a) {
+	document.submission.space.style.background = ((a)?'#FFFFE0':'white');
+}
+</script>
+</head>
+<body>
+<h1>BCA Math Competition Problem Submissions</h1>
+<?php
+printf("So far there are <span style=\"font-weight: 600;\">".file_get_contents("count.txt")."</span> problems in the database.");
+?>
+<form name="submission" method="post" action="<?php echo $PHP_SELF; ?>">
+<h3>Enter a Problem:</h3>
+<textarea rows="5" 
+			cols="80" 
+			wrap="soft" 
+			name="space" 
+			onclick="changeBg(1)" 
+			onBlur="changeBg(0)">
+</textarea>
+<br />
+<input type="submit" value="Submit" />
+</form>
+<?php
+if($submitted) {
+	printf("Thanks for your submission!\n");
+	$submitted = 0;
+}
+?>
+</body>
+</html>
---
+++ master.php
@@ -0,0 +1,62 @@
+<?php
+$a = str_ireplace("\n", "", trim(htmlspecialchars($_POST["space"])));
+if(strlen($a) != 0) 
+	file_put_contents("probs.txt", "[prob]".$a."[/prob]\n", FILE_APPEND);
+?>
+<html>
+<head>
+<title>PHP Test</title>
+<style type="text/css">
+body {
+	font: 10pt "Courier New";
+	width: 665px;
+}
+
+textarea {
+	font: 10pt "Courier New";
+}
+
+p {
+	border: thin dashed black;
+	padding: 5px;
+}
+
+p.dark {
+	background: #E6E6FA;
+}
+
+p.light {
+	background: #FFFAFA;
+}
+</style>
+<script type="text/javascript">
+function changeBg(a) {
+	document.submission.space.style.background = ((a)?'#FFFFE0':'white');
+}
+</script>
+</head>
+<body>
+<h2>BCA Math Competition Problem Submission Site</h2>
+<?php
+$contents = explode("\n", file_get_contents("probs.txt"));
+for($i = 0; $i < count($contents); $i++) {
+	if(substr($contents[$i], 0, 6) == "[prob]") {
+		$tmp = substr($contents[$i], 6, strlen($contents[$i])-13);
+		printf("<p class=\"".(($i%2==0)?"light":"dark")."\">Problem ".($i+1).": ".$tmp."</p>\n");
+	}
+}
+?>
+<form name="submission" method="post" action="<?php echo $PHP_SELF; ?>">
+<h3>Enter a Problem:</h3>
+<textarea rows="5" 
+			cols="80" 
+			wrap="soft" 
+			name="space" 
+			onclick="changeBg(1)" 
+			onBlur="changeBg(0)">
+</textarea>
+<br />
+<input type="submit" value="Submit" />
+</form>
+</body>
+</html>
