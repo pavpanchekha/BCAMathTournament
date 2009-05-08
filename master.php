acd2832fa2cb2ce9266ed3328a783a22d49d4a15
shewu <shewu19@gmail.com>
2009-05-08 17:34:09 -0400

    Now uses two text files for storage: one for 7th & 8th graders and one for 4th, 5th, 6th graders. There is a radio group for selecting the appropriate grade level. The PHP files will not let you submit if you don't choose a grade level!

--- master.php
+++ master.php
@@ -1,10 +1,23 @@
 <?php
 $a = str_ireplace("\n", "", trim(htmlspecialchars($_POST["space"])));
 $a = str_ireplace("\\\\", "\\", $a);
-if(strlen($a) != 0) 
+if(strlen($a) != 0) {
 	// back up existing file to probs.back.txt
-	file_put_contents("probs.back.txt", file_get_contents("probs.txt"));
-	file_put_contents("probs.txt", "[prob]".$a."[/prob]\n", FILE_APPEND);
+	if($_POST["agegroup"] == "young") {
+		file_put_contents("probs.young.back.txt", file_get_contents("probs.young.txt"));
+		file_put_contents("probs.young.txt", "[prob]".$a."[/prob]\n", FILE_APPEND);
+		$submitted = 1;
+	}
+	else if($_POST["agegroup"] == "old") {
+		file_put_contents("probs.old.back.txt", file_get_contents("probs.old.txt"));
+		file_put_contents("probs.old.txt", "[prob]".$a."[/prob]\n", FILE_APPEND);
+		$submitted = 1;
+	}
+	else {
+		printf("<script type=\"text/javascript\">alert(\"Please select a grade level\")</script>");
+		$submitted = 0;
+	}
+}
 ?>
 <html>
 <head>
@@ -27,16 +40,26 @@ function changeBg(a) {
 <?php
 
 // get # problems
-$arr = explode("\n", file_get_contents("probs.txt"));
+$arrO = explode("\n", file_get_contents("probs.old.txt"));
+$arrY = explode("\n", file_get_contents("probs.young.txt"));
 printf("So far there are <span style=\"font-weight:600;\">");
-printf(count($arr)-1);
+printf(count($arrO)+count($arrY)-2);
 printf("</span> problems in the database.");
 
-$contents = explode("\n", file_get_contents("probs.txt"));
-for($i = 0; $i < count($contents); $i++) {
+$contents = explode("\n", file_get_contents("probs.young.txt"));
+$i = 0;
+for(; $i < count($contents); $i++) {
 	if(substr($contents[$i], 0, 6) == "[prob]") {
 		$tmp = substr($contents[$i], 6, strlen($contents[$i])-13);
 		printf("<p class=\"".(($i%2==0)?"light":"dark")."\">Problem ".($i+1).": ".$tmp."</p>\n");
+	}
+}
+
+$contents = explode("\n", file_get_contents("probs.old.txt"));
+for($j = 0; $j < count($contents); $j++) {
+	if(substr($contents[$j], 0, 6) == "[prob]") {
+		$tmp = substr($contents[$j], 6, strlen($contents[$j])-13);
+		printf("<p class=\"".((($i+$j+1)%2==0)?"light":"dark")."\">Problem ".($i+$j).": ".$tmp."</p>\n");
 	}
 }
 ?>
